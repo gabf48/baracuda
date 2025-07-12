2f8a1e21df368f122a15cc1037547727a6af41a3
Gabriel Filip <gabf48@gmail.com>
2025-07-12 13:32:26 +0300

    enable headless

--- utils/browser_utils.py
+++ utils/browser_utils.py
@@ -6,7 +6,7 @@ class BrowserUtils:
     @staticmethod
     def init_driver():
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--window-size=1920,1080')
         options.add_argument('--disable-dev-shm-usage')
